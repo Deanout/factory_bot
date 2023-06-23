73d43eadadf6e691eddf88257fff71d2c9738605
Dean DeHart <DeHartDean@gmail.com>
2023-06-23 03:34:56 -0400

    Init commit

---
+++ spec/views/posts/show.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "posts/show", type: :view do
+  include Rails.application.routes.url_helpers
+
+  before(:each) do
+    @post = assign(:post, FactoryBot.create(:post))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/#{Regexp.escape(@post[:title])}/)
+    expect(rendered).to match(/#{Regexp.escape(@post[:content])}/)
+  end
+
+  it "renders the post image" do
+    render
+    assert_select "img[src=?]", rails_blob_path(@post.image, only_path: true), count: 1
+
+  end
+end
